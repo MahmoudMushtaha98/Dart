86db404db00ce7536fb8dae2a02f2c63401a9a0a
Mahmoud <modemushtaha@gmail.com>
2023-10-25 00:35:02 +0300

    task one

--- bin/task_one.dart
+++ bin/task_one.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 
 void main(List<String> arguments) {
@@ -11,7 +12,51 @@ void main(List<String> arguments) {
 //   evenOrOdd(number);
 // }
 
-  gradeCalculator();
+  // gradeCalculator();
+
+  guessGame();
+}
+
+void guessGame() {
+
+
+  print('We choose a random number between 1 - 100. Try to find out. Lets get started. '
+      '\nWe have chosen the number');
+
+//Here we have chosen a random number
+  Random random=Random();
+  int rand = random.nextInt(100);
+
+/*
+Here we have created an Infinite Loop that works until the number is known
+through the help. If the number is more than 50 far away, it will output
+ a message that it is very far away, but if it is less than 25, it will output
+  a message that it is close.
+ */
+  bool condition=true;
+  while(condition){
+
+    int number=int.parse(stdin.readLineSync()!);
+    int far =rand-number;
+
+    if(far>=50){
+      print('Too Far Choose a larger number');
+      continue;
+    }else if(far<=-50){
+      print('Too Far Choose a smaller number');
+      continue;
+    }else if(far>=1&&far<50){
+      print('It is close. The number is bigger than that');
+      continue;
+    }else if(far<=-1&&far>-50){
+      print('It is close. The number is smaller than that');
+      continue;
+    }else{
+      print('Yes, your answer is correct its : $rand');
+      condition=false;
+    }
+  }
+
 }
 
 void gradeCalculator() {
