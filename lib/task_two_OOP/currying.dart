ea45cdf09a5c65bb392d72125c960e829bd600d8
Mahmoud <modemushtaha@gmail.com>
2023-10-27 15:44:00 +0300

    OOP task

---
+++ lib/task_two_OOP/currying.dart
@@ -0,0 +1,13 @@
+
+
+int Function(int) curringAdd(int a){
+  return (int b){
+    return a+b;
+  };
+}
+
+  void main(){
+  int Function(int) functionOne=curringAdd(5);
+  int result = functionOne(4);
+  print(result);
+  }
